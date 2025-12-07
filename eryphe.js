39b71030ec52aa985d6d8c4b5d74c038cb751698
luCassporki <lucas.momot.rmz@gmail.com>
2025-12-07 18:18:03 +0100

    sun effect move3

--- eryphe.js
+++ eryphe.js
@@ -1,19 +1,14 @@
-
-
-
 function view() {
-     const content =document.getElementById("logo");
+  const content = document.getElementById("logo");
 
   // 2. Vérifier si le contenu est actuellement visible (a la classe 'active')
   if (content.classList.contains("sun")) {
     // Il est ouvert : on le ferme en retirant la classe
     content.classList.remove("sun");
     content.classList.add("moon");
-    
-} else {
+  } else {
     // Il est fermé : on l'ouvre en ajoutant la classe
     content.classList.remove("moon");
     content.classList.add("sun");
   }
-    
 }
