ff1e77fedb6b3ef302efe4440f952f12486b2a0d
luCassporki <lucas.momot.rmz@gmail.com>
2025-12-08 02:20:12 +0100

    sun effect

--- eryphe.js
+++ eryphe.js
@@ -1,14 +1,21 @@
 function view() {
   const content = document.getElementById("logo");
+  const contenttxt = document.getElementById("text");
 
   // 2. Vérifier si le contenu est actuellement visible (a la classe 'active')
   if (content.classList.contains("sun")) {
     // Il est ouvert : on le ferme en retirant la classe
     content.classList.remove("sun");
     content.classList.add("moon");
+
+    contenttxt.classList.remove("textOff");
+    contenttxt.classList.add("textOn");
   } else {
     // Il est fermé : on l'ouvre en ajoutant la classe
     content.classList.remove("moon");
     content.classList.add("sun");
+
+    contenttxt.classList.remove("textOn");
+    contenttxt.classList.add("textOff");
   }
 }
